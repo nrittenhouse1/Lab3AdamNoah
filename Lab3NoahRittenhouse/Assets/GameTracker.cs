08a1784f5e8bd64e34ef9ae0d9e74d77ab44f8b5
awarkentin <46605534+awarkentin@users.noreply.github.com>
2019-03-27 15:40:02 -0600

    yeet

---
+++ Lab3NoahRittenhouse/Assets/GameTracker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTracker : MonoBehaviour
+{
+    public static int numPlayers = 0;
+    public static bool team1Win = false;
+    public static bool team2Win = false;
+
+    public static int numGoons = 10; //number of goons still alive
+    public static int numTeam1 = 2; //number of players on team 1 that are still alive
+
+}
--- Lab3NoahRittenhouse/Assets/GameplayScripts/Player3Controls.cs
+++ Lab3NoahRittenhouse/Assets/GameplayScripts/Player3Controls.cs
@@ -23,9 +23,11 @@ public class Player3Controls : MonoBehaviour
     }
     playerState currentState = playerState.setSpawn;
 
+    public bool isAI;
+
     void Start()
     {
-
+        isAI = false;
         //Set camera
         player3Camera = transform.GetChild(0).GetComponent<Camera>();
         //Set HUD
@@ -41,7 +43,7 @@ public class Player3Controls : MonoBehaviour
             spawnpointButtons.Add(player3HUD.Find("SpawnButtons").transform.GetChild(i + 1));
         }
         goons = new List<GameObject>();
-        goonCount = 10;
+        goonCount = 50;
         goonPrefab = Resources.Load("Goon") as GameObject;
         goonParent = transform.Find("Goons");
         spawnpoint = null;
@@ -55,77 +57,142 @@ public class Player3Controls : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log(currentState);
-        switch (currentState)
+        if (Input.GetKeyDown(KeyCode.Backspace))
         {
-            case playerState.setSpawn:
-                if (doOnce)
-                {
-                    battleButton.GetComponent<Button>().interactable = false;
-                    doOnce = false;
-                }
-                break;
-            case playerState.beginBattle:
-                #region Spawnpoint visuals on overview screen
-                spIndicator.gameObject.SetActive(true);
-                spIndicator.position = virtualSpawnpoint.position;
-                #endregion
-                if (doOnce)
-                {
-                    battleButton.GetComponent<Button>().interactable = true;
-                    doOnce = false;
-                }
-                break;
-            case playerState.battleBegun:
-                if (doOnce)
-                {
-                    playerIndicator.gameObject.SetActive(false);
-                    battleButton.GetComponent<Button>().interactable = false;
-                    foreach (Transform button in spawnpointButtons)
+            isAI = !isAI;
+        }
+        Debug.Log("Is the AI on? " + isAI);
+        if (!isAI)//If AI is not on
+        {
+            switch (currentState)
+            {
+                case playerState.setSpawn:
+                    if (doOnce)
                     {
-                        button.gameObject.GetComponent<Button>().interactable = false;
+                        battleButton.GetComponent<Button>().interactable = false;
+                        doOnce = false;
                     }
-                    doOnce = false;
-                }
-                #region Player control area
-
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    MovementOrder(physicalSpawnpoints[3].position);
-                }
-                if (Input.GetMouseButtonDown(0))//If left click shoot ray from mouseclick and if it hits the floor send goons
-                {
-                    Vector3 mousePos = player3Camera.ScreenToWorldPoint(Input.mousePosition);
-                    RaycastHit ray;
-                    if (Physics.Raycast(mousePos, Vector3.down, out ray) && (ray.collider.tag == "Floor" || ray.collider.tag == "Player"))
+                    break;
+                case playerState.beginBattle:
+                    #region Spawnpoint visuals on overview screen
+                    spIndicator.gameObject.SetActive(true);
+                    spIndicator.position = virtualSpawnpoint.position;
+                    #endregion
+                    if (doOnce)
                     {
-                        GameObject marker = Instantiate(movementIndicatorPrefab, new Vector3(ray.point.x, ray.point.y + 50, ray.point.z), new Quaternion(0, 0, 0, 1), null);
-                        marker.transform.eulerAngles = new Vector3(90, 0, 0);
-                        MovementOrder(ray.point);
+                        battleButton.GetComponent<Button>().interactable = true;
+                        doOnce = false;
                     }
-                }
+                    break;
+                case playerState.battleBegun:
+                    if (doOnce)
+                    {
+                        playerIndicator.gameObject.SetActive(false);
+                        battleButton.GetComponent<Button>().interactable = false;
+                        foreach (Transform button in spawnpointButtons)
+                        {
+                            button.gameObject.GetComponent<Button>().interactable = false;
+                        }
+                        doOnce = false;
+                    }
+                    #region Player control area
+                    if (Input.GetMouseButtonDown(0))//If left click shoot ray from mouseclick and if it hits the floor send goons
+                    {
+                        Vector3 mousePos = player3Camera.ScreenToWorldPoint(Input.mousePosition);
+                        RaycastHit ray;
+                        if (Physics.Raycast(mousePos, Vector3.down, out ray) && (ray.collider.tag == "Floor" || ray.collider.tag == "Team1" || ray.collider.tag == "Team2"))
+                        {
+                            GameObject marker = Instantiate(movementIndicatorPrefab, new Vector3(ray.point.x, ray.point.y + 50, ray.point.z), new Quaternion(0, 0, 0, 1), null);
+                            marker.transform.eulerAngles = new Vector3(90, 0, 0);
+                            MovementOrder(ray.point, -1);
+                        }
+                    }
 
-                #endregion
-                break;
-            case playerState.battleOver:
-                break;
-            default:
-                break;
+                    #endregion
+                    break;
+                case playerState.battleOver:
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            battleButton.GetComponent<Button>().interactable = false;
+            foreach (Transform button in spawnpointButtons)
+            {
+                button.GetComponent<Button>().interactable = false;
+            }
+            playerIndicator.gameObject.SetActive(false);
+            switch (currentState)
+            {
+                case playerState.setSpawn://Can set spawn only
+                    int rnd = Random.Range(0, 3);
+                    SpawnLocation(rnd);
+                    break;
+                case playerState.beginBattle://Can set spawn or begin battle (AI will just begin)
+                    BeginBattle();
+                    break;
+                case playerState.battleBegun:
+                    //Find players
+                    GameObject[] players = GameObject.FindGameObjectsWithTag("Team1");
+                    if (doOnce)
+                    {
+                        for (int i = 0; i < goons.Count; i++)
+                        {
+                            if (i < goons.Count / 2)
+                            {
+                                MovementOrder(players[0].transform.position, i);
+                            }
+                            else
+                            {
+                                MovementOrder(players[1].transform.position, i);
+                            }
+                        }
+                        doOnce = false;
+                    }
+                    if(players.Length == 1)
+                    {
+                        if (players[0].gameObject.activeInHierarchy)
+                        {
+                            MovementOrder(players[0].transform.position, -1);
+                        }
+                        else
+                        {
+                            MovementOrder(players[1].transform.position, -1);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < goons.Count; i++)
+                        {
+                            if (i < goons.Count / 2)
+                            {
+                                MovementOrder(players[0].transform.position, i);
+                            }
+                            else
+                            {
+                                MovementOrder(players[1].transform.position, i);
+                            }
+                        }
+                    }
+                    break;
+                case playerState.battleOver:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
     public void SpawnLocation(int choice)
     {
-        if (Input.GetMouseButtonUp(0))
-        {
-            //Choices are 0-3
-            spawnpoint = physicalSpawnpoints[choice];
-            goonParent.localPosition = spawnpoint.localPosition;
-            virtualSpawnpoint = virtualSpawnpoints[choice];//Just for visuals
-            doOnce = true;
-            currentState = playerState.beginBattle;
-
-        }
+        //Choices are 0-3
+        spawnpoint = physicalSpawnpoints[choice];
+        goonParent.localPosition = spawnpoint.localPosition;
+        virtualSpawnpoint = virtualSpawnpoints[choice];//Just for visuals
+        doOnce = true;
+        currentState = playerState.beginBattle;
     }
 
     public void BeginBattle()
@@ -149,11 +216,18 @@ public class Player3Controls : MonoBehaviour
         }
     }
 
-    public void MovementOrder(Vector3 destination)
+    public void MovementOrder(Vector3 destination, int goonToOrder)
     {
-        foreach (GameObject goon in goons)
+        if (goonToOrder < 0)
         {
-            goon.GetComponent<NavMeshAgent>().SetDestination(destination);
+            foreach (GameObject goon in goons)
+            {
+                goon.GetComponent<NavMeshAgent>().SetDestination(destination);
+            }
+        }
+        else
+        {
+            goons[goonToOrder].GetComponent<NavMeshAgent>().SetDestination(destination);
         }
     }
 }
